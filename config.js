39390817d820556d6442bb2db9b1ea14af36b850
Bryan Giang <bgiang1234567890@gmail.com>
2026-04-18 21:13:26 -0700

    Point frontend config at local backend

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
-const isLocalHost =
-  window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-
 window.APP_CONFIG = {
-  apiBaseUrl: isLocalHost ? 'http://localhost:3001' : '',
+  apiBaseUrl: 'http://localhost:3001',
   createJobPath: '/jobs',
   defaultStatusPath: '/jobs/{jobId}',
   uploadMethod: 'PUT'
